8b19010b0bacb1ffb1fc09c432840a69abcc56b7
anangyoga <aharwinto@gmail.com>
2022-12-17 21:19:29 +0700

    learn: implement getstaticprops fn for dummy

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,22 @@ import Hero from "../components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Home({ movies }) {
+  // this will return the array of the movies from the getStaticProps fn
+  console.log(movies);
   return (
     <>
       <Hero />
     </>
   );
 }
+
+export async function getStaticProps() {
+  const movies = [{ name: "hello" }];
+
+  return {
+    props: { movies },
+  };
+}
+
+// https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
